5e5937de05ff4deb3f87c4e4470c6d0162ac5187
Hiroki72110 <guy72110@gmail.com>
2023-12-16 13:43:13 +0800

    pico_w

--- Pico_W/pico9_2.py
+++ Pico_W/pico9_2.py
@@ -1,13 +1,21 @@
 from machine import Timer
 
-def callback1(t):
+def callback1(t:Timer):
     print(1)
 
-def callback2(t):
+def callback2(t:Timer):
     print(2)
+    
+def callback3(t:Timer):
+    print(3)
+    t.deinit()
+
        
 time1 = Timer()
 time1.init(freq = 1, callback = callback1)
 
-time1 = Timer()
-time1.init(period = 2000, callback = callback2)    
+time2 = Timer()
+time2.init(period = 2000, callback = callback2)
+
+time3 = Timer()
+time3.init(period = 3000, callback = callback3)    
