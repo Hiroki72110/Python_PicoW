9ecbfd44295cbc9816c81c79475605bfe23c36ac
Hiroki72110 <guy72110@gmail.com>
2023-12-16 14:10:33 +0800

    pico_w

--- Pico_W/pico10_1.py
+++ Pico_W/pico10_1.py
@@ -1,10 +1,11 @@
 from machine import ADC, Timer
+# Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree.
 
 def	callback1(t:Timer):
-    temperature = ADC(4)
-    print(temperature.read_u16())
-    vol = temperature.read_u16() * (3.3/65535)
-    print(vol)
+    sensor = ADC(4)
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706)/0.001721
+    print(temperature)
     
 time1 = Timer()
 time1.init(period = 1000, callback = callback1)
